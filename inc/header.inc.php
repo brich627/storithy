2012c1343215448d7959ff9d61e6743b194cdab3
Brian Richardson <35270779+brich627@users.noreply.github.com>
2018-12-26 04:16:17 -0500

    Create header.inc.php

---
+++ inc/header.inc.php
@@ -0,0 +1,43 @@
+<?php 
+/*include("./inc/connect.inc.php");
+session_start();
+if (!isset($_SESSION["user_login"])){
+}
+else
+{
+	$username = $_SESSION["user_login"];
+}*/
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<title>Storithy | file sharing community</title>
+		<link rel="stylesheet" type="text/css" href="./css/style.css" />
+                <link rel="icon" 
+      type="image/png" 
+      href="./img/str_favicon.bmp">
+	</head>
+<body>
+
+	<div class="headerMenu">
+		<div id="wrapper">
+			<div class="logo">
+				<img src="./img/str.png" />
+			</div>
+			<!--<div class="search_box">
+			<form action="search.php" method="GET" id="search">
+				<input type="text" name="q" size="50" placeholder="search..."/>
+			</form>
+			</div> -->
+			<div id="menu">
+				<a href="../home.php" />Home</a>
+				<a href="../about.php" />About</a>
+				<a href="../index.php" />Log in</a>
+				<a href="../regpage.php" />Sign up!</a>
+                                <a href="../donate.php" />Donate</a>
+			
+			</div>
+		</div>
+	</div>
+	
